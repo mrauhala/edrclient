fb24e9ce6f246e470d7ccce2943956a9569cb960
Mikko Rauhala <mikko@rauhala.net>
2025-11-01 22:31:35 +0200

    feat: Add lint and lint:fix npm scripts
    - Add 'npm run lint' script to check code quality with ESLint
    - Add 'npm run lint:fix' script to auto-fix linting issues
    - Update CI workflow to use the lint command
    - Fix ESLint warning in axios mock file

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,4 +1,4 @@
-export default {
+const axiosMock = {
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
   put: jest.fn(() => Promise.resolve({ data: {} })),
@@ -6,3 +6,5 @@ export default {
   patch: jest.fn(() => Promise.resolve({ data: {} })),
   request: jest.fn(() => Promise.resolve({ data: {} })),
 };
+
+export default axiosMock;
