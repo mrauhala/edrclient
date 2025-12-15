27c27a2ef50d46665323414d1e93a8fdeac32124
Mikko Rauhala <mikko@rauhala.net>
2025-12-15 16:01:16 +0200

    Fix: Update Meteogate Climate Observations URL to include versioning

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -88,7 +88,7 @@ const edrServices = [
   { label: '[EDR] SWIM SMHI', value: 'https://aviation.smhi.se' },
   { label: '[EDR] Met Office Labs', value: 'https://labs.metoffice.gov.uk/edr' },
   { label: '[EDR] Meteogate Observations', value: 'https://api.meteogate.eu/eu-eumetnet-surface-observations' },
-  { label: '[EDR] Meteogate Climate Observations', value: 'https://api.meteogate.eu/eu-eumetnet-climate-observations' },
+  { label: '[EDR] Meteogate Climate Observations', value: 'https://api.meteogate.eu/eu-eumetnet-climate-observations/v1' },
   { label: '[EDR] SmartMet Kenya', value: 'https://data-kenya.smartmet.org/edr' },
   { label: '[EDR] SmartMet Ethiopia', value: 'https://data-ethiopia.smartmet.org/edr' },
   { label: '[Records] WMO GDC WIS2 Germany', value: 'https://wis2.dwd.de/gdc/' },
